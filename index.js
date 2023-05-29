651578eca284002c6eea396d8c1c79ab150146dd
rafin8990 <rafin8990@gmail.com>
2023-05-30 03:57:23 +0600

    new code added

--- index.js
+++ index.js
@@ -11,17 +11,18 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nuouh7o.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const moduleCollection = client.db('lms-client').collection('modules');
 async function run() {
     try {
-// get all modules 
+        const moduleCollection = client.db('lms-client').collection('modules');
+        const serviceCollection = client.db('lms-client').collection('services')
+        // get all modules 
         app.get('/modules', async (req, res) => {
             const query = {};
             const result = await moduleCollection.find(query).toArray();
             res.send(result)
         });
 
-        app.put('/modules/lessons/:index', async (req, res) => {
+        /* app.put('/modules/lessons/:index', async (req, res) => {
             const index = parseInt(req.params.index);
             try {
                 const result = await moduleCollection.updateOne({ index }, { $set: { unlocked: true } });
@@ -34,6 +35,26 @@ async function run() {
                 console.error('Error unlocking video:', error);
                 res.status(500).json({ error: 'An error occurred' });
             }
+        }) */
+
+        app.get('/services', async (req, res) => {
+            const query={}
+            const result = await serviceCollection.find(query).limit(3).toArray();
+            res.send(result);
+        });
+
+        app.get('/allservices', async (req, res) => {
+            const query={}
+            const result = await serviceCollection.find(query).toArray();
+            // console.log(result);
+            res.send(result);
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await serviceCollection.findOne(query)
+            res.send(result)
         })
 
     }
